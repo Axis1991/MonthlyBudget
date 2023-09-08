6fc537501aeea012dfd3be8f6958c91c5f9bdc47
Grzesiek <gromaniuk0@gmail.com>
2023-09-08 12:01:00 +0200

    to reset

--- models.py
+++ models.py
@@ -16,9 +16,3 @@ class Shopping(object):
         # def __post_init__(self):
     #     if float(self.value) < 0:
     #         raise ValueError("Value cannot be negative")
-    #     if not self.item:
-    #         raise ValueError("You need to specify the name")
-    #     if len(self.username) < 3:
-    #         raise ValueError("Username needs at least 3 characters")
-    #     if not self.password:
-    #         raise ValueError("Password cannot be empty")
