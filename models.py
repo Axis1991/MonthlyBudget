b68fcea2c7101f1777b4a1d7d40cd8411dd4e21e
Grzesiek <gromaniuk0@gmail.com>
2023-09-08 13:29:30 +0200

    Najsu

--- models.py
+++ models.py
@@ -22,3 +22,6 @@ class Shopping(object):
     #         raise ValueError("Username needs at least 3 characters")
     #     if not self.password:
     #         raise ValueError("Password cannot be empty")
+
+
+    "This is so amazing"
